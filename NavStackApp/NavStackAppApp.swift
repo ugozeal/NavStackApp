6ef9f0dd31d8dcd6aa3b74e83b086cb739b4df9e
David Okonkwo <okonkwodavidu@gmail.com>
2023-02-11 18:23:47 +0100

    Initial Commit

---
+++ NavStackApp/NavStackAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavStackAppApp.swift
+//  NavStackApp
+//
+//  Created by David Okonkwo on 11/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavStackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
